ffe9a2601b9e9556caacf6340faf923dcc4c8aaf
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-07 23:09:42 +0900

    fix GameMenuLoop

--- GameMenu.cpp
+++ GameMenu.cpp
@@ -120,7 +120,8 @@ void GameMenuLoop() noexcept {
 	while (EndFlag == 17) {
 		ScreenClear();
 		GameMenuDraw(GameMenuCursorPosY);
-		GameMenuCursorPosY = (ConfigData.MouseAndKeyFlag == 1) ? GameMenuMouseMove(GameMenuCursorPosY) : GameMenuKeyMove(GameMenuCursorPosY);
+		GameMenuCursorPosY = (ConfigData.MouseAndKeyFlag == 1) ? MouseMoveTemplate(GameMenuCursorPosY, GameMenuBasePosY, CursorMove, 12, ConfigData.MouseAndKeyFlag)
+																	: KeyMoveTemplate(GameMenuCursorPosY, GameMenuBasePosY, GameMenuBasePosY * 12, CursorMove, ConfigData.MouseAndKeyFlag);
 		GameMenuSelect(GameMenuCursorPosY);
 
 		//ゲーム終了確認ウインドウ
